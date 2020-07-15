39bcbeadec12af7a4fcecd6ad1dfe7d4306d490d
hendra-agil <agilsyaputra26@gmail.com>
2020-07-15 15:57:36 +0700

    View Engines

--- app.js
+++ app.js
@@ -3,25 +3,42 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+// "views" is default dir name
+app.set('view engine', 'ejs');
+// if dir name not "views"
+// app.set('views', 'myviews');
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  // res.send('<p>Homepage</p>');
-  res.sendfile('./views/index.html', { root: __dirname });
+  const blogs = [
+    {
+      title: 'Yoshi finds eggs',
+      snippet: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      title: 'Mari finds stars',
+      snippet: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      title: 'How to defeat monsters',
+      snippet: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<p>About Page</p>');
-  res.sendfile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a New Blog' });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendfile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', { title: '404 Not Found!' });
 });
