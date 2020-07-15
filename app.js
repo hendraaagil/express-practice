33b7000d6bc9eb24a9b59de6deece282b93a391b
hendra-agil <agilsyaputra26@gmail.com>
2020-07-15 17:39:16 +0700

    Middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -11,6 +12,24 @@ app.set('view engine', 'ejs');
 
 // listen for request
 app.listen(3000);
+
+// app.use((req, res, next) => {
+//   console.log('new request made');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('in the next middleware');
+//   next();
+// });
+
+// middleware & static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   const blogs = [
