ffc570954b572a2345f38d2e8d48e6cc87816c38
hendra-agil <agilsyaputra26@gmail.com>
2020-07-15 19:09:10 +0700

    Mongoose and Mongo Sandbox Routes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
+
+// connect to mongodb
+const dbURI =
+  'mongodb+srv://hendra:agil123@nodetuts.did91.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 // "views" is default dir name
@@ -10,27 +20,49 @@ app.set('view engine', 'ejs');
 // if dir name not "views"
 // app.set('views', 'myviews');
 
-// listen for request
-app.listen(3000);
-
-// app.use((req, res, next) => {
-//   console.log('new request made');
-//   console.log('host: ', req.hostname);
-//   console.log('path: ', req.path);
-//   console.log('method: ', req.method);
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log('in the next middleware');
-//   next();
-// });
-
 // middleware & static files
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
 
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog two',
+    snippet: 'about my new blog',
+    body: 'more about my new blog',
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+  Blog.findById('5f0ef04691e4070ab8ab7f5f')
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// routes
 app.get('/', (req, res) => {
   const blogs = [
     {
