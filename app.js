b9548a294597d34611755b4bf1b039a36eb8c4eb
hendra-agil <agilsyaputra26@gmail.com>
2020-07-17 21:06:41 +0700

    Get & Post Requests

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // routes
@@ -39,6 +40,19 @@ app.get('/blogs', (req, res) => {
     .sort({ createdAt: -1 })
     .then((result) => {
       res.render('index', { title: 'All Blogs', blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
     })
     .catch((err) => {
       console.log(err);
