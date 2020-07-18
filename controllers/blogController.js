f5eb62531caccbd06046e46942f9b066f3d262f4
hendra-agil <agilsyaputra26@gmail.com>
2020-07-18 12:49:18 +0700

    Fix 404 Page

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -19,7 +19,7 @@ const blog_details = (req, res) => {
       res.render('blogs/details', { blog: result, title: 'Blog Details' });
     })
     .catch((err) => {
-      console.log(err);
+      res.status(404).render('404', { title: '404 Not Found!' });
     });
 };
 
