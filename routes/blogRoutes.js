e1c0b00cabfac805efee0e46a1a8275c45babc6d
hendra-agil <agilsyaputra26@gmail.com>
2020-07-18 12:30:45 +0700

    Models, Views, Controllers (MVC)

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,57 +1,12 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.post('/', (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect('/blogs');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.get('/create', (req, res) => {
-  res.render('create', { title: 'Create a New Blog' });
-});
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', { blog: result, title: 'Blog Details' });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: '/blogs' });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/create', blogController.blog_create_get);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
